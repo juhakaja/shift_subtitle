b9176aa7a8ea7f576538f106093e7583083b2313
Juha Kajava <juhakaja@gmail.com>
2012-07-12 20:34:55 +0900

    Clean up srt_reader.

--- lib/srt_reader.rb
+++ lib/srt_reader.rb
@@ -3,9 +3,14 @@ class SrtReader
 
   def initialize filename
     @filename = filename
+    @subtitle_container = []
   end
 
   def load_file
-    File.read @filename
+    File.read(@filename).chomp
+  end
+
+  def by_block
+    load_file.split(/\n\n/)
   end
 end
--- spec/srt_reader_spec.rb
+++ spec/srt_reader_spec.rb
@@ -3,15 +3,33 @@ require 'spec_helper'
 describe SrtReader do
 
   let(:filename) { "Foo Bar" }
-  let(:content) { "foobar" }
   let(:srt_reader) { SrtReader.new filename }
+  let(:content) { 
+    <<-eos.gsub(/^ {6}/, '')
+      645
+      01:31:51,210 --> 01:31:54,893
+      the government is implementing a new policy...
 
-  it 'should read file' do
+      646
+      01:31:55,412 --> 01:31:56,192
+      to raise taxes,
+      ad infinitum
+    eos
+  }
+
+  it "should read file" do
     File.stub(:read).with(filename).and_return content
-    srt_reader.load_file.should == content
+    srt_reader.load_file.should == content.chomp
   end
 
-  it 'should have a filename' do
+  it "should have a filename" do
     srt_reader.filename.should == filename
+  end
+
+  it "should should return content blocks in array" do
+    File.stub(:read).with(filename).and_return content
+    blocks = srt_reader.by_block
+    blocks.should be_an_instance_of Array
+    blocks.first.should == content.split(/\n\n/).first
   end
 end
