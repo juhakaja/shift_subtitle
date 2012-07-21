d841571ee29df393d1de76d41e45340588140e02
Juha Kajava <juhakaja@gmail.com>
2012-07-21 22:51:05 +0900

    Create srt_builder module to build srt from file.

--- bin/shift_subtitle.rb
+++ bin/shift_subtitle.rb
@@ -1,23 +1,7 @@
 
 require 'lib/subtitles'
+require 'lib/srt_builder'
 
-def testing
-  file = 'tmp/subtitles.srt'
+srt = SrtBuilder::build 'tmp/subtitles.srt'
 
-  content = SrtIO::Reader.new(file).by_block
-
-  srt_sub = SrtSubtitles.new("my_subtitles") do |srt_block|
-    content.each do |block| 
-      block = block.split("\n")
-      lineno = block[0]
-      time_start, time_end = block[1].split(" --> ")
-      content = block.drop(2).join("\n")
-
-      subtitle = SrtBlock.new lineno, time_start, time_end, content
-
-      srt_block << subtitle
-    end
-  end
-
-  return srt_sub
-end
+SrtIO::Writer.new('tmp/s2.srt') { srt }
---
+++ lib/srt_builder.rb
@@ -0,0 +1,18 @@
+module SrtBuilder
+  def self.build file
+    content = SrtIO::Reader.new(file).by_block
+
+    srt_sub = SrtSubtitles.new("my_subtitles") do |srt_block|
+      content.each do |block| 
+        block = block.split("\n")
+        lineno = block[0]
+        time_start, time_end = block[1].split(" --> ")
+        content = block.drop(2).join("\n")
+
+        subtitle = SrtBlock.new lineno, time_start, time_end, content
+
+        srt_block << subtitle
+      end
+    end
+  end
+end
